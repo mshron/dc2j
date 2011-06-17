43090c0bb25199f64b82cb3f14c0ff72fdbf71bd
Max Shron <max.shron@gmail.com>
2011-06-16 21:46:02 -0400

    Newer versions of scraper

--- scrape_articles.py
+++ scrape_articles.py
@@ -80,10 +80,10 @@ def main():
     files = recurse_subdirectories('www.aspendailynews.com')
     files = recurse_subdirectories('www.suntimes.com')
 
-    pt = (39.9205411,-105.0866504)
-    pt = (41.7003713,-73.9209701)
-    pt = (39.1910983,-106.8175387)
-    pt = (41.887610,-87.636057)
+    pt = (39.9205411, -105.0866504)
+    pt = (41.7003713, -73.9209701)
+    pt = (39.1910983, -106.8175387)
+    pt = (41.887610,  -87.636057)
 
     places = parse_location_file()
     place_re = placefinder.make_re(places)
@@ -96,7 +96,6 @@ def main():
     print c_nearest
     cov = coverage(pt, c_nearest)
     print cov
-
 
 if __name__ == "__main__":
     main()
