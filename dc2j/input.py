55b1c8819b9e25cf75a57b8751a7a3e2c62dc4eb
Max Shron <max.shron@gmail.com>
2011-06-26 12:34:56 -0400

    Friendsourcing works.

--- dc2j/input.py
+++ dc2j/input.py
@@ -1,6 +1,7 @@
 '''Interactive friendsourcing tool'''
 
 from model import *
+import logging
 import os
 from random import random
 from google.appengine.ext.webapp import template
@@ -24,8 +25,34 @@ class AddJournalists(webapp.RequestHandler):
         htmlpath = os.path.join(os.path.dirname(__file__), 'addjournalist.html')
         self.response.out.write(template.render(htmlpath, {'n': n, 'range': range(3)}))
 
-    def put(self):
-       return 
+    def post(self):
+        nid = self.request.get('nid')
+        _n = Newspaper.all().filter('nid =', nid).fetch(1)
+        if _n: n = _n[0]
+        else:
+            logging.error("%s was created then not found!"%nid)
+            self.redirect('/input/jounralists')
+        error = self.request.get('error')
+        if error != None:
+            n.error = error
+            n.put()
+            self.redirect('/input/journalists') 
+        for r in xrange(3):
+            email = self.request.get('email-%s'%r)
+            name = self.request.get('name-%s'%r)
+            isperson = self.request.get('isperson-%s'%r)
+            if email != "":
+                jid = hex(randint(0,2**32))[2:] 
+                j = Journalist(jid = jid)
+                j.email = email 
+                j.threshold = .5
+                j.isPerson = isperson=='on'
+                j.fullName = name
+                j.put()
+                n.journalists.append(j.key())
+                n.put()
+
+        self.redirect('/input/journalists')
         
         
 
